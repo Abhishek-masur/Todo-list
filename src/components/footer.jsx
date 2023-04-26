d36ac18a28e6ef4eaf958406d94dd0e905ce72dc
Abhishek-masur <83652156+Abhishek-masur@users.noreply.github.com>
2023-04-26 23:43:19 +0530

    update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
-  let footerStyle={
-    position:"relative",
-    top:"vh",
-    width:"100%"
-  }
-  //comment above class afterwards
+  // let footerStyle={
+  //   position:"relative",
+  //   top:"vh",
+  //   width:"100%"
+  // }
+
   return (
 
 
